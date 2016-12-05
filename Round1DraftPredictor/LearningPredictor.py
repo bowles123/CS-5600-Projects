2985563fc3335b80b75471303147f0b901ee2155
Brian <Brian>
2016-12-05 16:57:09 -0700

    Added beginnings of neural network.

--- Round1DraftPredictor/LearningPredictor.py
+++ Round1DraftPredictor/LearningPredictor.py
@@ -1,7 +1,12 @@
-import pybrain
+from pybrain.supervised.trainers import BackpropTrainer
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.structure import TanhLayer
+from pybrain.datasets import SupervisedDataSet
+
+draftResults = {}
 
 positionMap = {
-    'QB': '1000000000', 'RB': '0100000000', 'WR': '0010000000', 'TE': '0001000000',
+    'QB': '1000000000', 'RB': '100000000', 'WR': '0010000000', 'TE': '0001000000',
     'OT': '0000100000', 'OG': '0000100000', 'DL': '0000010000', 'LB': '0000001000',
     'DB': '0000000100', 'P': '0000000010', 'K': '0000000001', 'OL': '0000100000',
     'DE': '0000010000', 'DT': '0000010000', 'CB': '0000000100', 'S': '0000000100',
@@ -28,6 +33,13 @@ orderMap = {
     '29': '11110111111111111111111111111111', '30': '11101111111111111111111111111111',
     '31': '11011111111111111111111111111111', '32': '10111111111111111111111111111111'}
 
+def stringToList(string):
+    lst = []
+    
+    for char in string:
+        lst.append(int(char))
+    return lst
+
 class Prospect:
     
     def __init__(self, name, position, stats, combine, score = 0.0):
@@ -41,7 +53,7 @@ class Prospect:
         if prospect == None:
             return False
 
-        if prospect.name == self.name && prospect.position == self.position:
+        if prospect.name == self.name and prospect.position == self.position:
             return True
         return False
 
@@ -71,20 +83,67 @@ class Team:
         return string
 
 class LearningPredictor:
-    def __init__(testSet, trainSet):
-        self.testSet = []
-        self.trainingSet = []
-        self.getData(testSet, trainSet)
+    def __init__(self, testSet):
+        self.testData = {}
+        self.trainingData = {}
+        self.getData(testSet)
+        self.neuralNet = buildNetwork(2, 3, 1, bias=True,
+                                 hiddenclass=TanhLayer)
 
-    def getData(testFile, trainingTestFile):
-        self.testSet = getTestSet(testFile , "2016")
-        self.trainingSet = getTrainingSet(["2015", "2014", "2013"])
+    def getData(self, testFile):
+        self.testData = getDataSet(testFile , "2016")
+        self.trainingData = getTrainingSet(["2015", "2014", "2013"])
 
-    def train():
+    def train(self): 
+        trainer = BackpropTrainer(self.neuralNet, self.buildDataSet())
+        error = trainer.trainUntilConvergence()
+        print(error)
+
+    def predict(self):
+        teams = self.testData.get('2016')[0]
+        prospects = self.testData.get('2016')[1]
+
+        for team in teams:
+            for prospect in prospects:
+                if self.neuralNetwork.activate():
+                    team.pick = prospect
+                    continue
+
+    def calculateAccuracy(self):
+        results = draftResults[2016]
+        correct = i = 0
         
+        for team in self.teams:
+            if team.pick.equals(results[i].pick):
+                correct = correct + 1
+            i = i + 1
+        return (correct / len(self.teams)) * 100
 
-    def predict():
+    def display_predictions(self):
+        for team in self.teams:
+            if team.pick == None:
+                print("%s: None." % (team.name))
+            else:
+                print("%d. %s: %s %s" % (team.pickNum, team.name,
+                                         team.pick.position,
+                                         team.pick.name))
+
+    def buildDataSet(self):
+        dataSet = SupervisedDataSet(5, 1)
         
+        for year in self.trainingData:
+            for key, value in year.items():
+                tpl = value
+
+                for team in tpl[0]:
+                    for prospect in tpl[1]:
+                        dataSet.addSample([int(prospect.position in team.needs),
+                                           stringToList(prospect.position),
+                                           stringToList(team.pickNum),
+                                           prospect.stats,
+                                           prospect.combine],
+                        (int(prospect.equals(team.pick)),))
+        return dataSet
 
 def getTrainingSet(years):
     train = []
